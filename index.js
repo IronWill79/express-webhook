ddbbfdbfc56a6286801ac673ede922a5d2737355
Will Walsh <1820392+IronWill79@users.noreply.github.com>
2021-11-30 11:48:36 +1000

    Add shell execution and test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { exec } = require('child_process');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,7 +9,22 @@ const PORT = process.env.WEBHOOK_PORT;
 app.use(bodyParser.json());
 
 app.post(`/hook/${process.env.WEBHOOK_PATH}`, (req, res) => {
-  res.status(200).send({ message: 'OK', recievedMessage: req.body });
+  exec('ls -la', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      res.status(500).send(error.message);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      res.status(500).send(stderr);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    res
+      .status(200)
+      .send({ message: 'OK', recievedMessage: req.body, response: stdout });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
