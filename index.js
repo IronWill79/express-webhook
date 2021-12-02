cce280791c5df07ea188a91bb0573d3ddbf9ece9
Will Walsh <1820392+IronWill79@users.noreply.github.com>
2021-12-02 10:24:07 +1000

    Add rebuild catch

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.WEBHOOK_PORT;
+let rebuilding = false;
 
 app.use(bodyParser.json());
 
 app.post(`/hook/:path`, (req, res) => {
-  console.log(req.body);
   if (req.params.path !== process.env.WEBHOOK_PATH) {
     return;
   }
-  if (process.env.TESTING !== false) {
+  if (process.env.TESTING !== false && !rebuilding) {
+    rebuilding = true;
     exec(process.env.SHELL_SCRIPT, (error, stdout, stderr) => {
       if (error) {
         console.error(JSON.stringify(error));
@@ -24,9 +25,12 @@ app.post(`/hook/:path`, (req, res) => {
         return;
       }
       console.log(JSON.stringify(stdout));
+      rebuilding = false;
     });
+    res.status(200).send({ message: 'Rebuild started' });
+  } else {
+    res.status(200).send({ message: 'Rebuilding' });
   }
-  res.status(200).send({ message: 'OK' });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
